7bcaf75c5ae38cda3affa3e899f0af5ee4ce6ad8
fransiskalala <fransiskalalalalala@gmail.com>
2022-03-08 22:53:51 +0700

    construct

--- application/controllers/Aaaaa.php
+++ application/controllers/Aaaaa.php
@@ -4,6 +4,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Aaaaa extends CI_Controller {
 
+	
+	public function __construct()
+	{
+		parent::__construct();
+		//mencoba
+	}
+	
 	public function index()
 	{
 		
