e41449cb9dd8abc37093cf3b0d6c612b9518a9d6
fransiskalala <fransiskalalalalala@gmail.com>
2022-03-09 14:07:50 +0700

    upload foto baru dan ganti bbbbback dan fffffront

--- application/views/admin/template/footer.php
+++ application/views/admin/template/footer.php
@@ -1,17 +1,17 @@
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="<?= base_url() ?>bbbbback/assets/vendor/apexcharts/apexcharts.min.js"></script>
-  <script src="<?= base_url() ?>bbbbback/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="<?= base_url() ?>bbbbback/assets/vendor/chart.js/chart.min.js"></script>
-  <script src="<?= base_url() ?>bbbbback/assets/vendor/echarts/echarts.min.js"></script>
-  <script src="<?= base_url() ?>bbbbback/assets/vendor/quill/quill.min.js"></script>
-  <script src="<?= base_url() ?>bbbbback/assets/vendor/simple-datatables/simple-datatables.js"></script>
-  <script src="<?= base_url() ?>bbbbback/assets/vendor/tinymce/tinymce.min.js"></script>
-  <script src="<?= base_url() ?>bbbbback/assets/vendor/php-email-form/validate.js"></script>
+  <script src="<?= base_url() ?>backend/assets/vendor/apexcharts/apexcharts.min.js"></script>
+  <script src="<?= base_url() ?>backend/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?= base_url() ?>backend/assets/vendor/chart.js/chart.min.js"></script>
+  <script src="<?= base_url() ?>backend/assets/vendor/echarts/echarts.min.js"></script>
+  <script src="<?= base_url() ?>backend/assets/vendor/quill/quill.min.js"></script>
+  <script src="<?= base_url() ?>backend/assets/vendor/simple-datatables/simple-datatables.js"></script>
+  <script src="<?= base_url() ?>backend/assets/vendor/tinymce/tinymce.min.js"></script>
+  <script src="<?= base_url() ?>backend/assets/vendor/php-email-form/validate.js"></script>
 
   <!-- Template Main JS File -->
-  <script src="<?= base_url() ?>bbbbback/assets/js/main.js"></script>
+  <script src="<?= base_url() ?>backend/assets/js/main.js"></script>
 
 
   <script>
