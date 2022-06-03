ba32513f788e664dbd111b62729ed3e7ae6f7d8f
fransiskalala <fransiskalalalalala@gmail.com>
2022-06-03 21:14:49 +0700

    Ganti config.php dan database.php

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 date_default_timezone_set('Asia/Jakarta');
-$config['base_url'] = 'http://localhost/aguaria';
+$config['base_url'] = 'http://www.aguaria.co.id';
 
 /*
 |--------------------------------------------------------------------------
